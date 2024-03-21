4627a7740785e241ff671855de86d4a93b352b88
himjoshi95 <114509321+himjoshi95@users.noreply.github.com>
2024-03-21 19:58:43 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".room-slider",{
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
